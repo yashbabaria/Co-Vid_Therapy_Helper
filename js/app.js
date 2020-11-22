f42b8927d4f3f06120f41a7f69c319d7013324d5
Yash Babaria <32756653+yashbabaria@users.noreply.github.com>
2020-11-21 17:19:47 -0700

    created a hard code alg for the survey

--- js/app.js
+++ js/app.js
@@ -8,78 +8,141 @@ form.addEventListener('submit', function(e){
 
 function surveyResult() {
   const age = document.getElementById('Age')
+  var young = ""
   if(age.value < 18){
-    location.href = '../OrgSitePages/TeenCounselingOrgInfo.html'
-    return
+    young = "true"
   }
-
+  var hasInsurance = ""
   const insur = document.getElementById('dropdown')
   if(insur.value === 'yes'){
-    //has insurance
+    hasInsurance = "yes"
   }else if(insur.value === 'no'){
-    // does not have insurance
+    hasInsurance == "no"
   }
 
+  var price = ""
   const cost = document.getElementsByName('price')
   if(cost[0].checked){
-    //less
+    price = "low"
   }else if(cost[1].checked){
-    //middle
+    price = "mid"
   }else if(cost[2].checked){
-    //high
+    price = "high"
   }else if(cost[3].checked){
-    //insurance will cover
+    price = "any"
   }
 
+  var help = 0;
   const mental = document.getElementsByName('mental')
   if(mental[0].checked){
-    //Anxiety
+    help++
   }
   if(mental[1].checked){
-    //Depression
+    help++
   }
   if(mental[2].checked){
-    //Stress Management
+    help++
   }
   if(mental[3].checked){
-    //LGBTQ Couseling
+    if(price != "low"){
+      location.href = "../OrgSitePages/PrideCounselingOrgInfo.html"
+      return
+    }
+    help++
   }
   if(mental[4].checked){
-    //Grief
+    help++
   }
   if(mental[5].checked){
-    //OCD
+    help++
   }
   if(mental[6].checked){
-    //Couple or Relationship Therapy
+    if(price != "low"){
+      location.href = "../OrgSitePages/RegainOrgInfo.html"
+      return
+    }
+    help++
   }
   if(mental[7].checked){
-    //PTSD/Trauma
+    help++
   }
   if(mental[8].checked){
-    //Insomnia
+    help++
   }
   if(mental[9].checked){
-    //Life-Transition
+    help++
   }
 
+  var psych = ""
   const care = document.getElementsByName('care')
   if(care[0].checked){
-    // Need of Psychiatric Care
+    psych = "yes"
+  }
+  if(psych == "yes"){
+    if(price == "high" || price == "any"){
+      if(help < 2){
+        location.href = "../OrgSitePages/MDLiveOrgInfo.html"
+        return
+      }else if(help < 5){
+        location.href = "../OrgSitePages/AmwellOrgInfo.html"
+        return
+      }else{
+        location.href = "../OrgSitePages/DrOnDemandOrgInfo.html"
+        return
+      }
+    }
   }
 
+  var video = ""
   const online = document.getElementsByName('online')
   if(online[0].checked){
-    // Wants a Video Conference
+    video = "true"
   }else if(online[1].checked){
-    // Wants to only Message
+    video = "false"
   }else if(online[2].checked){
-    // Wants Both
+    video = "either"
   }
+
 
   const vg = document.getElementsByName('game')
   if(vg[0].checked){
-    // plays video games
+    location.href = "../OrgSitePages/HealthyGamerOrgInfo.html"
+    return
+  }
+
+  if(young == "true"){
+    if(price == "low"){
+      location.href = "../OrgSitePages/7CupsOrgInfo.html"
+      return
+    }else{
+      location.href = "../OrgSitePages/TeenCounselingOrgInfo.html"
+      return
+    }
+  }
+
+  var rand = 0
+  if(price == "low"){
+    location.href = "../OrgSitePages/7CupsOrgInfo.html"
+    return
+  }else if(price == "mid"){
+    if(help < 3){
+      location.href = "../OrgSitePages/TalkSpaceOrgInfo.html"
+      return
+    }else{
+      location.href = "../OrgSitePages/BetterHelpOrgInfo.html"
+      return
+    }
+  }else if(price == "high"){
+    if(help < 2){
+      location.href = "../OrgSitePages/MDLiveOrgInfo.html"
+      return
+    }else if(help < 5){
+      location.href = "../OrgSitePages/AmwellOrgInfo.html"
+      return
+    }else{
+      location.href = "../OrgSitePages/DrOnDemandOrgInfo.html"
+      return
+    }
   }
 
   location.href = "../OrgSitePages/TalkSpaceOrgInfo.html"
